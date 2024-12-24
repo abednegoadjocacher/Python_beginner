79efddf9e411a7caffa9d178b459b138d2cf544c
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2024-12-24 09:56:23 +0000

    Number swaping

--- LearnPython/helloworld.py
+++ LearnPython/helloworld.py
@@ -1,1 +1,19 @@
 print("Hello world")
+
+#Accept user input and store it with variable name.
+#print the user input and find the length.
+
+#name = input("What is your name? ")
+#print(name)
+#lengthOfName =len(name)
+#print(lengthOfName)
+
+#Exercise: write a program to swap two numbers
+#Eg a =2; b=3
+numberOne =10
+numberTwo =12
+NumberSwap=numberOne
+numberOne=numberTwo
+numberTwo=NumberSwap
+print("numberOne = ",numberOne)
+print("numberTwo = ", numberTwo)
