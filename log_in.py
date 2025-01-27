4cb7ad831c5a06f5be3e97a30fbd33a3921f63a2
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-01-27 00:50:47 +0000

    Login page in python

---
+++ log_in.py
@@ -0,0 +1,15 @@
+'''
+A simple log in page for user
+The must enter name and password validation
+'''
+username = input("Enter user name: ")
+password = input("Enter user password: ")
+print("Account created successfully!\nLog in Now")
+username_1 =input("Enter user name: ")
+password_1 = input("Enter password: ")
+# Check if user name and password  is correct
+if username_1==username and password_1==password:
+    print("Log in successfully")
+else:
+    print("Access denial")
+
