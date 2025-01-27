c5581f30bad623a2ee5e7cd4e5788dfaeb793967
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-01-27 00:52:35 +0000

    File handling in python

---
+++ file_handling.py
@@ -0,0 +1,8 @@
+count_file=open("all_text.txt", "r")
+print(count_file.readable()) # Check if the file is readable. It returns true if readable. Else otherwise
+print(count_file.readline())# Reads the file content by single line only
+#print(count_file.readlines())# Reads the entire file and print it out as list
+
+for file in count_file.readlines():
+    print(file)
+count_file.close()
