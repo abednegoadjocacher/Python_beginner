b9a43b513609c94fe6d0009395708047e13700c0
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-05-08 22:00:16 +0000

    Print list  items

--- list.py
+++ list.py
@@ -2,10 +2,12 @@ markOfStudents=[50,32,13,56,0,-9,-7,-1,46,78,90,11] #This is List
 markOfStudents.append(45) #To add a single number to the list
 markOfStudents.extend([2,3]) #To add multiple numbers
 print(markOfStudents) #To print the list
-print(markOfStudents[:7:2])
-print(markOfStudents[3]) #To print a specific mark
-print("The length of the list :",len(markOfStudents))
-markOfStudents.sort()
-print("Sorted result:",markOfStudents)
-markOfStudents.reverse()
-print("The reversed order:",markOfStudents)
+for mark in markOfStudents:
+    print(mark) # printing each item in the list on new line
+# print(markOfStudents[:7:2])
+# print(markOfStudents[3]) #To print a specific mark
+# print("The length of the list :",len(markOfStudents))
+# markOfStudents.sort()
+# print("Sorted result:",markOfStudents)
+# markOfStudents.reverse()
+# print("The reversed order:",markOfStudents)
